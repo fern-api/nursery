0ce3cdaa830e055980b42020286130a0e68b07b0
dsinghvi <deep@usebirch.com>
2022-09-26 20:24:35 -0700

    ingress from within vpc

--- nursery-cdk/src/nurseryCdkStack.ts
+++ nursery-cdk/src/nurseryCdkStack.ts
@@ -36,6 +36,8 @@ export class NurseryCdkStack extends Stack {
       allowAllOutbound: true,
     });
 
+    nurserySg.addIngressRule(Peer.ipv4("172.31.0.0/16"), Port.allTcp());
+
     const cluster = Cluster.fromClusterAttributes(this, "cluster", {
       clusterName: config.ecsClusterName,
       vpc,
